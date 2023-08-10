4735e86996c761945e6f8f1a5caa8f09e1e1c4fe
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-10 12:43:22 +0300

    Saving my progress

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
-/* Your Code Here */
+
+function createEmployeeRecord(employeeRecord){
+
+   const [firstName,familyName,title,payPerHour] = employeeRecord;
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents:[]
+    };
+}
+//const employeeRecord = createEmployeeRecord(["Gray", "Worm", "Security", 1]);
+//console.log(employeeRecord)
+
+function createEmployeeRecords(employeeRecords){
+  return  employeeRecords.map(employeeRecord =>createEmployeeRecord(employeeRecord));
+
+
+}
+ 
+let employeeRecords = [
+    ["moe", "sizlak", "barkeep", 2],
+    ["bartholomew", "simpson", "scamp", 3]
+  ];
+
+  let nameExtractor = function (e) { return e.firstName ;}
+ let employeeName= createEmployeeRecords(employeeRecords).map(nameExtractor);
+ console.log(employeeName);
+ let x= createEmployeeRecords(employeeRecords)
+ console.log(x)
+
+function createTimeInEvent(dateStamp){
+
+    const [date,time] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time, 10),
+        date: date
+    });
+    return this;
+  
+}
+let employeeRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3]);
+let updatedemployeeRecord = createTimeInEvent.call(employeeRecord, "2014-02-28 1400");
+let newEvent = updatedemployeeRecord.timeInEvents[0]
+console.log(updatedemployeeRecord)
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
